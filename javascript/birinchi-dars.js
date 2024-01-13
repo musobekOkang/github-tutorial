c77bbc2960e85e35833b5773c9fe5a5f137e15fe
Your Name <you@example.com>
2024-01-13 15:47:10 +0300

    first commit

---
+++ javascript/birinchi-dars.js
@@ -0,0 +1,44 @@
+"use strict";
+
+let number = 3.2; //data type number
+
+
+
+// const place = confirm("qayerdasan hozir? ");
+// console.log(place);
+// const xabar = alert('assalomu alaykum') //undefined qaytardi
+// console.log(typeof xabar);
+// const xabar  = confirm('hush kelibsiz') //boolean qaytardi
+// console.log(xabar);
+// const xabar = prompt('yoshingiz nechida?',"") //string qaytardi
+// console.log(typeof xabar);
+// const malumot = +prompt('yoshingiz nechida?',"") //number qaytardi
+// console.log(typeof malumot);
+
+// const toplam = [] //bo'sh ,assiv
+// toplam[0] = "ko`k" // massivga element qo'shish
+// console.log(toplam); 
+
+// const toplam = [] //bo'sh massiv
+// toplam[0] = prompt("yaxshi korgan rangingizni kiriting #1","");  //promt orqali massivga element qoshdik
+// toplam[1] = prompt("yaxshi korgan rangingizni kiriting #2",""); //promt orqali massivga element qoshdik
+// toplam[2] = prompt("yaxshi korgan rangingizni kiriting #3",""); //promt orqali massivga element qoshdik
+// console.log(toplam); //massivga element qo'shildi
+// console.log(typeof toplam); //turi object ekan
+
+// const channel = prompt("qaysi youtuberni ko`rishni xohlaysiz ?", "")
+// console.log("https://youtube/"+ channel );
+
+// const user = "samar";
+// const job = "javascript developer";
+// console.log(`my name is ${user} he is ${job}`);
+let h = 10
+
+function f(a) {
+  h = a * a;
+  return h ;
+}
+console.log(f(5));
+
+
+
